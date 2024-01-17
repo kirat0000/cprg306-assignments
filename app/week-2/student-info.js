37465383224411f754bbd963731216ece3b75893
Jaskirat Singh <jaskiratsingh@Jaskirats-MacBook-Air-2.local>
2024-01-17 11:09:58 -0700

    added week-2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function StudentInfo() {
+    return (
+      <main>
+        <p>Jaskirat Singh</p>
+        <Link href="https://github.com/kirat0000">This is my github profile.😀😀😀 </Link>
+      </main>
+    );
+  }
