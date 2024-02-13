92c83cb6898598db45a0c2d8b702ab9a89f50fd4
Jaskirat Singh <131496205+kirat0000@users.noreply.github.com>
2024-02-13 09:10:23 -0700

    Add files via upload

---
+++ app/week-4/page.js
@@ -0,0 +1,10 @@
+
+import NewItem from './new-item';
+
+export default function Page() {
+    return (
+        <div className="min-h-screen bg-gradient-to-br from-purple-400 to-indigo-500 flex justify-center items-center">
+            <NewItem />
+        </div>
+    );
+}
