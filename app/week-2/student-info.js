6030149abb6a997a8f245c8ab8b2e114218c8918
Jaskirat Singh <131496205+kirat0000@users.noreply.github.com>
2024-02-26 20:58:21 -0700

    Update student-info.js

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -2,8 +2,8 @@ import Link from "next/link";
 export default function StudentInfo() {
     return (
       <main>
-        <p>Jaskirat Singh</p>
-        <Link href="https://github.com/kirat0000">This is my github profile.😀😀😀 </Link>
+        <p>Student Name - Jaskirat Singh </p>
+        <p>Click here to access my gitub Profile 👉🏻 <Link  href="https://github.com/kirat0000"> <a className="underline text-orange-600 hover:text-purple-400">KIRAT0000</a></Link> 😀</p>
       </main>
     );
   }
