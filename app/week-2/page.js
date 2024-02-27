92e203241503a9705f1fcf3e3e20314462499662
Jaskirat Singh <131496205+kirat0000@users.noreply.github.com>
2024-02-26 20:57:51 -0700

    Update page.js

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,5 +1,13 @@
 import StudentInfo from "./student-info";
 export default function Page() {
+  return (
+    <main className="min-h-screen bg-gradient-to-r from-green-400 to-blue-500 p-10">
+        <div className="max-w-4xl mx-auto rounded-lg shadow-lg bg-white p-6">
+            <h1 className="text-3xl font-bold text-gray-800 border-b pb-4">Shopping List</h1>
+            <StudentInfo />
+        </div>
+    </main>
+);
     return (
       <main className="min-h-screen">
 
