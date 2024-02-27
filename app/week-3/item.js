6ac80549eac999b5edba5e5c5888f9c828f62833
Jaskirat Singh <131496205+kirat0000@users.noreply.github.com>
2024-02-26 20:59:20 -0700

    Update item.js

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,7 +1,11 @@
+
 const Item = ({name,quantity,category}) => {
-    return(<li className="p-2 m-4 bg-slate-900 max-w-sm">
-        <h1 className="text-xl font-bold">{name}</h1>
-        <div className="text-sm">Buy {quantity} in {category}</div>
+    return(<li className="p-4 m-2 bg-white bg-opacity-90 rounded-lg shadow-md hover:shadow-lg transition duration-300 ease-in-out flex justify-between items-center">
+    <div>
+        <h1 className="text-xl font-bold text-teal-600">{name}</h1>
+        <div className="text-sm text-gray-700">Buy {quantity} in {category}</div>
+        </div>
     </li >)
+    
 }
 export default Item;
