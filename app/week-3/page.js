300d5adee53be7ad4079eec1337dc274da97bd9d
Jaskirat Singh <131496205+kirat0000@users.noreply.github.com>
2024-02-26 20:59:47 -0700

    Update page.js

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,15 +1,15 @@
 import items from "./item-list";
 import Item from "./item";
-export default function Page() {
-    return (
-      <main className="min-h-screen bg-slate-950 text-white flex flex-col">
-        <h1 className="text-3xl font-bold m-2">Shopping List</h1>
-        <ul>
 
-        {items.map((iteem,index)=>(
-            <Item key={index} name={iteem.name} quantity={iteem.quantity} category={iteem.category}></Item>
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-gradient-to-r from-blue-500 to-teal-500 text-white-800 flex flex-col items-center py-8">
+      <h1 className="text-4xl font-extrabold text-white mb-5 shadow-lg p-3 rounded-lg bg-opacity-80 bg-black">Shopping List</h1>
+      <ul className="w-full max-w-4xl">
+        {items.map((item, index) => (
+          <Item key={index} name={item.name} quantity={item.quantity} category={item.category}></Item>
         ))}
-        </ul>
-      </main>
-    );
-  }
+      </ul>
+    </main>
+  );
+}
